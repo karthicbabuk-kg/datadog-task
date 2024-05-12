11ae01c6bf3e4c49a7abdf5650df176dd2f006cf
karthicbabuk-kg <karthicbabu333@gmail.com>
2024-05-12 13:33:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "./App.css"
-const apiKey = '#######';
+const apiKey = '#######';//API key is private so i removed
 const App = () => {
   const [monitors, setMonitors] = useState([]);
 
